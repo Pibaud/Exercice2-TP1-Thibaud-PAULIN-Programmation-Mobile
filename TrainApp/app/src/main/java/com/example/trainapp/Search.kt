057da47a972748189ec02e9e24bc42598a1d66e4
Pibaud <thibaud.paulin@gmail.com.com>
2026-02-04 16:56:00 +0100

    debut app train

---
+++ TrainApp/app/src/main/java/com/example/trainapp/Search.kt
@@ -0,0 +1,10 @@
+package com.example.trainapp
+
+import android.app.Activity
+import android.os.Bundle
+
+class Search: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+}
